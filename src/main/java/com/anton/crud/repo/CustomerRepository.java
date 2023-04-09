87f99b9028b425b7c15bc9156dc608c0b6316392
Piyumi Navodani <navodanip97@gmail.com>
2023-04-09 20:23:10 +0530

    customer remove

--- src/main/java/com/anton/crud/repo/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anton.crud.repo;
-
-import com.anton.crud.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository <Customer, Long> {
-}
